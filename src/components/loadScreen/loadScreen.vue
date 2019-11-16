bdaa406815d22634c1eb2b8721354dbdb11f7391
Sebastian Bauder <bauderse68422@th-nuernberg.de>
2019-11-16 22:53:39 +0100

    added import function

--- src/components/loadScreen/loadScreen.vue
+++ src/components/loadScreen/loadScreen.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="load-screen">
-        <p>Ich bin die Auswahl</p>
+        <p>Select adventure source</p>
         <div class="button-container">
-            <button @click="loadAdventure">import</button>
-            <button class="load-button" @click="loadAdventure">load</button>
+            <button class="import-button" @click="importAdventure">import</button>
+            <button class="load-button" @click="notImplemented">load</button>
+            <input class="file-upload" type="file" @change="handleUpload"/>
         </div>
     </div>
 </template>
@@ -12,8 +13,23 @@
     export default {
         name: "loadScreen",
         methods: {
-            loadAdventure() {
-                this.$store.commit("loadAdventure")
+            notImplemented() {
+                alert("This function is not implemented yet")
+            },
+            importAdventure(clickEvent) {
+                document.querySelector('.file-upload').click()
+                clickEvent.preventDefault()
+            },
+            handleUpload(changeEvent) {
+                const uploadedFile = changeEvent.target.files[0]
+                uploadedFile.text()
+                    .then(adventureJson => {
+                        const uploadedAdventure = JSON.parse(adventureJson)
+                        // TODO:    check if uploaded adventure complies with the scheme of an adventure object
+                        //          (no clue how to do that but this is neccessary in order to avoid uploading something that is not an adventure)
+
+                        this.$store.commit('setAdventureObject', uploadedAdventure)
+                    })
             }
         }
     }
@@ -23,9 +39,44 @@
     .load-screen {
         padding: 2em;
         background-image: radial-gradient(#fff,#d8d8d8);
+        border-radius: 1.2em;
+
+        font-family: var(--font);
+    }
+
+    .button-container {
+
+    }
+
+    .import-button {
+        outline-style: none;
+        border-radius: 0.4em 0.4em 0.4em 0.4em;
+        border: 2px solid #d9d9d9;
+        background-color: #f2f2f2;
+        font-size: 1.3em;
+        cursor: pointer;
     }
 
     .load-button {
         float: right;
+
+        outline-style: none;
+        border-radius: 0.4em 0.4em 0.4em 0.4em;
+        border: 2px solid #d9d9d9;
+        background-color: #f2f2f2;
+        font-size: 1.3em;
+        cursor: pointer;
+    }
+
+    .load-button:hover {
+        border-color: #6c9dc6;
+    }
+
+    .import-button:hover {
+        border-color: #6c9dc6;
+    }
+
+    .file-upload {
+        display: none;
     }
 </style>
--- src/store/store.js
+++ src/store/store.js
@@ -5,16 +5,18 @@ Vue.use(Vuex)
 
 export const store = new Vuex.Store({
     state: {
+        adventureObject: {},
         expandStatus: false,
         adventureLoaded: false
     },
     mutations: {
+        setAdventureObject(state, adventure) {
+            state.adventureObject = adventure
+            state.adventureLoaded = true;
+        },
         toggleStatus(state) {
             state.expandStatus = !state.expandStatus
         },
-        loadAdventure(state) {
-            state.adventureLoaded = true;
-        }
     },
     actions: {}
 })
